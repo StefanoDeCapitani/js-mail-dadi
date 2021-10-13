a8c8ecdcf1100ae3d2d962ce9a28d33bd85b869c
Stefano De Capitani <stefano.deca@live.com>
2021-10-13 16:42:20 +0200

    Short version added to avoid multiple breaks.

--- js/access_by_email.js
+++ js/access_by_email.js
@@ -1,4 +1,4 @@
-const emails = [
+/* const emails = [
   "console.log@virgilio.it",
   "mario.rossi@live.com",
   "alice.manzoni94@yahoo.it",
@@ -8,13 +8,13 @@ const emails = [
 ];
 const attemptsAvailable = 3;
 let message;
+let emailRecognized = false;
 
-for (let j = 0; j < attemptsAvailable; j++) {
+ for (let j = 0; j < attemptsAvailable; j++) {
   let attempt = j + 1;
   const emailEntry = prompt("Scrivi la tua mail per accedere.");
 
-  let emailRecognized = false;
-  if (emailEntry) {
+   if (emailEntry) {
     for (let i = 0; i < emails.length; i++) {
       if (emailEntry.toLowerCase() === emails[i]) {
         emailRecognized = true;
@@ -32,15 +32,60 @@ for (let j = 0; j < attemptsAvailable; j++) {
         "Email non riconosciuta.\nTentativi esauriti. Riprova più tardi.";
       break;
     }
+    message = `Email non riconosciuta.\nTentativo di accesso ${attempt}/${attemptsAvailable}.\n\nPremi "OK" per riprovare.`;
+    alert(message);
 
-    alert(
-      `Email non riconosciuta.\nTentativo di accesso ${attempt}/${attemptsAvailable}.\n\nPremi "OK" per riprovare.`
-    );
   } else {
+
     message =
       "Accesso annullato. Per riprovare è necessario ricaricare la pagina.";
     break;
+
   }
 }
 
-alert(message);
+alert(message); */
+
+//Short version. Avoided multiple breaks.
+
+const emails = [
+  "console.log@virgilio.it",
+  "mario.rossi@live.com",
+  "alice.manzoni94@yahoo.it",
+  "guido.mariani@gmail.com",
+  "enrico.prestigiacomo@live.com",
+  "viola.bellini00@gmail.com",
+];
+
+let message;
+let emailRecognized = false;
+let attemptsAvailable = 3;
+
+while (!emailRecognized && attemptsAvailable > 0) {
+  const emailEntry = prompt("Scrivi la tua mail per accedere.");
+  attemptsAvailable--;
+
+  if (emailEntry === null) {
+    alert(
+      "Accesso annullato. Per riprovare è necessario ricaricare la pagina."
+    );
+    break;
+  } else {
+    if (attemptsAvailable === 0) {
+      message =
+        "Email non riconosciuta.\nTentativi esauriti. Riprova più tardi.";
+    } else {
+      message = `Email non riconosciuta.\nHai ancora ${attemptsAvailable} tentativi.\n\nPremi "OK" per riprovare.`;
+    }
+
+    for (let i = 0; i < emails.length; i++) {
+      if (emailEntry.toLowerCase() === emails[i]) {
+        emailRecognized = true;
+        message = "Accesso consentito. La tua mail è stata riconosciuta.";
+        break;
+      }
+    }
+  }
+
+  alert(message);
+}
---
+++ js/roll_the_dice.js
@@ -0,0 +1,3 @@
+const diceFaces = [1, 2, 3, 4, 5, 6];
+
+for (let i = 0; i < diceFaces.length; i++) {}
