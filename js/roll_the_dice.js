5c57b0acf49a0fe81a3e9488e1cb62ee9dff93c8
Stefano De Capitani <stefano.deca@live.com>
2021-10-13 18:05:20 +0200

    "Roll the dice" initiated

--- js/roll_the_dice.js
+++ js/roll_the_dice.js
@@ -1,3 +1,47 @@
-const diceFaces = [1, 2, 3, 4, 5, 6];
+const rollBtn = document.querySelector(".btn-roll");
 
-for (let i = 0; i < diceFaces.length; i++) {}
+let computerRoll = 0,
+  computerScore = 0;
+let userRoll = 0,
+  userScore = 0;
+
+const computerDice = document.querySelector(".game-board__computer .dice");
+const userDice = document.querySelector(".game-board__user .dice");
+
+const computerScoreDisplay = document.querySelector(
+  ".game-board__computer .score"
+);
+const userScoreDisplay = document.querySelector(".game-board__user .score");
+computerScoreDisplay.textContent = computerScore;
+userScoreDisplay.textContent = userScore;
+
+let message = "";
+
+rollBtn.addEventListener("click", function () {
+  computerRoll = Math.round(Math.random() * 5 + 1);
+  userRoll = Math.round(Math.random() * 5 + 1);
+  computerDice.textContent = computerRoll;
+  userDice.textContent = userRoll;
+  if (computerRoll !== userRoll) {
+    if (computerRoll > userRoll) {
+      computerScore++;
+      computerScoreDisplay.textContent = computerScore;
+      if (userScore === 10) {
+        message = "You won!";
+        rollBtn.disable = true;
+      }
+    } else {
+      userScore++;
+      userScoreDisplay.textContent = userScore;
+      if (computerScore === 10) {
+        message = "You lost!";
+        rollBtn.disable = true;
+      }
+    }
+  } else {
+    message = "Draw!";
+  }
+
+  console.log(computerRoll, userRoll);
+  console.log(computerScore, userScore);
+});
