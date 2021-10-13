cddcc12a6dfd03d9cd28f4f1cdc3bfb90a4b4ded
Stefano De Capitani <stefano.deca@live.com>
2021-10-13 15:29:34 +0200

    Max 3 attempts available

--- js/access_by_email.js
+++ js/access_by_email.js
@@ -6,17 +6,37 @@ const emails = [
   "enrico.prestigiacomo@live.com",
   "viola.bellini00@gmail.com",
 ];
-let message = "Email non riconosciuta, accesso negato";
+const attemptsAvailable = 3;
 
-const emailEntry = prompt("Scrivi la tua mail per accedere.");
-if (emailEntry) {
-  emailRiconosciuta = emails.includes(emailEntry.toLowerCase());
+for (let j = 0; j < attemptsAvailable; j++) {
+  let attempt = j + 1;
+  let message = `Email non riconosciuta, tentativo di accesso ${attempt}/${attemptsAvailable}\nPremi "OK" per riprovare.`;
 
-  if (emailRiconosciuta) {
-    message = "Accesso consentito. La tua mail è stata riconosciuta.";
+  const emailEntry = prompt("Scrivi la tua mail per accedere.");
+
+  let emailRecognized = false;
+  if (emailEntry) {
+    for (let i = 0; i < emails.length; i++) {
+      if (emailEntry.toLowerCase() === emails[i]) {
+        emailRecognized = true;
+        break;
+      }
+    }
+
+    if (emailRecognized) {
+      message = "Accesso consentito. La tua mail è stata riconosciuta.";
+      break;
+    }
+
+    if (attempt === attemptsAvailable) {
+      message = "Tentativi esauriti. Riprova più tardi.";
+    }
+
+    alert(message);
+  } else {
+    alert(
+      "Accesso annullato. Per riprovare è necessario ricaricare la pagina."
+    );
+    break;
   }
-
-  alert(message);
-} else {
-  alert("Accesso annullato. Per riprovare è necessario ricaricare la pagina.");
 }
