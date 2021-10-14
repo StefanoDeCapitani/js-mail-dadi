8505f3377eefb6af8181591cc190435b2a2b5764
Stefano De Capitani <stefano.deca@live.com>
2021-10-14 14:40:31 +0200

    Roll the dice done

--- js/roll_the_dice.js
+++ js/roll_the_dice.js
@@ -1,4 +1,9 @@
 const rollBtn = document.querySelector(".btn-roll");
+const messageImg = document.querySelector(".message-image");
+
+/* messageImg.setAttribute("src", "img/roll-the-dice.png"); */
+
+const MAX_NUMBER_OF_ROLLS = 10;
 
 let computerRoll = 0,
   computerScore = 0;
@@ -11,6 +16,8 @@ const computerDice = document.querySelector(
 const userDice = document.querySelector(
   ".game-board__user .dice .dice-content"
 );
+
+const dices = document.querySelectorAll(".dice");
 
 computerDice.style.gridTemplateColumns = "repeat(3, 1fr)";
 computerDice.style.gridTemplateRows = "repeat(3, 1fr)";
@@ -29,53 +36,80 @@ const gameBoardMessage = document.querySelector(".game-board__message");
 let message = "";
 
 rollBtn.addEventListener("click", function () {
-  computerRoll = Math.round(Math.random() * 5 + 1);
-  userRoll = Math.round(Math.random() * 5 + 1);
-  while (computerDice.firstChild) {
-    computerDice.removeChild(computerDice.lastChild);
-  }
-  while (userDice.firstChild) {
-    userDice.removeChild(userDice.lastChild);
-  }
+  clearElement(computerDice);
+  clearElement(userDice);
 
-  if (computerRoll !== userRoll) {
-    if (computerRoll > userRoll) {
-      computerScore++;
-      computerScoreDisplay.textContent = computerScore;
-      if (userScore === 10) {
-        message = "You won!";
-        rollBtn.disabled = true;
-      }
-    } else {
-      userScore++;
-      userScoreDisplay.textContent = userScore;
-      if (computerScore === 10) {
-        message = "You lost!";
-        rollBtn.disabled = true;
-      }
-    }
-    computerRoll = numberToDotsArray(computerRoll);
-    for (let i = 0; i < 9; i++) {
-      let dot = document.createElement("div");
-      dot.classList.add("dot");
-      if (computerRoll.includes(i + 1)) {
-        dot.classList.add("visible");
-      }
-      computerDice.append(dot);
-    }
-    userRoll = numberToDotsArray(userRoll);
-    for (let i = 0; i < 9; i++) {
-      let dot = document.createElement("div");
-      dot.classList.add("dot");
-      if (userRoll.includes(i + 1)) {
-        dot.classList.add("visible");
-      }
-      userDice.append(dot);
-    }
-  } else {
-    message = "Draw!";
-  }
+  rollAllDices();
+
+  setScores();
+  displayScores();
+
+  checkForWin(computerScore, userScore);
+  stopWhenScoreReachesMax(computerScore, userScore);
+
+  appendDiceDotsToDiceContent(computerDice, computerRoll);
+  appendDiceDotsToDiceContent(userDice, userRoll);
 });
+
+function clearElement(dice) {
+  while (dice.firstChild) {
+    dice.removeChild(dice.lastChild);
+  }
+}
+
+function rollAllDices() {
+  computerRoll = rollDice();
+  userRoll = rollDice();
+}
+
+function rollDice() {
+  return Math.round(Math.random() * 5 + 1);
+}
+
+function setScores() {
+  if (computerRoll > userRoll) {
+    computerScore++;
+  } else if (computerRoll < userRoll) {
+    userScore++;
+  }
+}
+
+function displayScores() {
+  resetScoresColorAndFontSizes();
+  computerScoreDisplay.textContent = computerScore;
+  userScoreDisplay.textContent = userScore;
+  if (computerRoll > userRoll) {
+    computerScoreDisplay.style.background = "#B0E298";
+    computerScoreDisplay.style.fontSize = "2rem";
+  }
+  if (computerRoll < userRoll) {
+    userScoreDisplay.style.background = "#B0E298";
+    userScoreDisplay.style.fontSize = "2rem";
+  }
+  if (computerRoll === userRoll) {
+    dices.forEach((el) => (el.style.background = "#B0E298"));
+  }
+}
+
+function resetScoresColorAndFontSizes() {
+  computerScoreDisplay.style.background = "#f9f9f9";
+  userScoreDisplay.style.background = "#f9f9f9";
+  computerScoreDisplay.style.fontSize = "1rem";
+  userScoreDisplay.style.fontSize = "1rem";
+  dices.forEach((el) => (el.style.background = "#f9f9f9"));
+}
+
+function appendDiceDotsToDiceContent(dice, roll) {
+  roll = numberToDotsArray(roll);
+  for (let i = 0; i < 9; i++) {
+    let dot = document.createElement("div");
+    dot.classList.add("dot");
+    if (roll.includes(i + 1)) {
+      dot.classList.add("visible");
+    }
+    dice.append(dot);
+  }
+}
 
 function numberToDotsArray(roll) {
   const one = [5];
@@ -106,4 +140,30 @@ function numberToDotsArray(roll) {
       break;
   }
   return rollArray;
+}
+
+function checkForWin(computerScore, userScore) {
+  if (computerScore === MAX_NUMBER_OF_ROLLS) {
+    displayWin();
+  }
+  if (userScore === MAX_NUMBER_OF_ROLLS) {
+    displayLost();
+  }
+}
+
+function displayWin() {
+  messageImg.setAttribute("src", "img/you-lost.png");
+}
+
+function displayLost() {
+  messageImg.setAttribute("src", "img/you-win.png");
+}
+
+function stopWhenScoreReachesMax(computerScore, userScore) {
+  maxNumberOfRollsReached =
+    computerScore === MAX_NUMBER_OF_ROLLS || userScore === MAX_NUMBER_OF_ROLLS;
+  if (maxNumberOfRollsReached) {
+    rollBtn.disabled = true;
+    rollBtn.style.color = "rgba(61, 59, 142, 0.3)";
+  }
 }
